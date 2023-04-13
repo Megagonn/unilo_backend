7b8e42748cca157f7ca1edb3cbff0f7c64a34a35
Yusuff Rasheed <yusuffrasheed.yr@gmail.com>
2023-04-13 09:52:08 +0100

    unilo

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const express = require('express')
 const app = express();
 const cors = require('cors');
 const {mysqlConnect} = require('./db/db');
+const bodyParser = require('body-parser')
 const userRoutes = require('./routes/user.route');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+
+///middlewares
+app.use(cors());
+app.use("/user", userRoutes);
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 ///env
 require("dotenv").config();
 
 const port = process.env.PORT || 5860;
 
+app.get('/', (req, res) => res.send('Hello World!'));
 mysqlConnect.connect((err) => {
     var query = "create database IF NOT EXISTS unilo_db;"
     if (err) {
@@ -25,9 +32,7 @@ mysqlConnect.connect((err) => {
     })
 })
 
-///middlewares
-app.use(cors());
-app.use("/user", userRoutes);
+
 
 
 app.listen(port, () => console.log(`Unilo app listening on port ${port}!`));
