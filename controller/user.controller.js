09f2494753feaf243d842ac134a1c645621212d2
Yusuff Rasheed <yusuffrasheed.yr@gmail.com>
2023-04-13 14:35:19 +0100

    update db

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,27 +2,25 @@ const Model = require('../model/user.model');
 const { sequelize } = require('../db/db');
 
 const signup = (req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
     try {
         sequelize.sync().then(() => {
             Model.findOne({
                 where: {
                     email: req.body.email
                 },
-
             }).then((result) => {
                 if (result) {
                     res.send("Email already exists!");
                     res.end();
                 } else {
-
                     Model.create({
                         fname: " ",
                         lname: " ",
                         phone: req.body.phone || "",
                         email: req.body.email || "",
-                        imageURL: "https://",
-                        token: "1fs252r1-sdf-fd---sd-dsf-s",
+                        imageURL: "",
+                        token: "",
                     });
                     res.status(200).send({ res: req.body });
                 }
@@ -33,7 +31,39 @@ const signup = (req, res) => {
     }
 };
 
-const getall = (req, res) => {
+const addName = (req, res) => {
+    try {
+        Model.findOne({
+            where: { email: req.body.email }
+        }).then(result => {
+            if (result) {
+                var fname = req.body.fname;
+                var lname = req.body.lname;
+                try {
+                    Model.update({
+                        "fname": fname,
+                        "lname": lname,
+                    }, {
+                        where: { email: req.body.email}
+                    }).then(result => {
+                        if (result) {
+                            res.status(200).send({ res: result });
+                            
+                        }
+                    });
+                } catch (error) {
+
+                }
+            } else {
+                res.send("Email not found!");
+            }
+        });
+    } catch (error) {
+        res.send(`Something went wrong! \n\n${error.message}`);
+    }
+};
+
+const getall = (_, res) => {
     try {
         sequelize.sync().then(() => {
             Model.findAll().then((result) => {
@@ -45,4 +75,4 @@ const getall = (req, res) => {
     }
 };
 
-module.exports = { signup, getall };
+module.exports = { signup, getall, addName };
--- routes/user.route.js
+++ routes/user.route.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const userController = require("../controller/user.controller")
 
 router.post("/signup", userController.signup);
+router.put("/addname", userController.addName);
 router.get("/get", userController.getall);
 
 module.exports = router;
